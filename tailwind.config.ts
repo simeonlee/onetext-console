dbe22da967bcd8d59b7abc407208a1190990923d
simeonlee <15081451+simeonlee@users.noreply.github.com>
2024-06-26 21:11:28 -0400

    Add high resolution border class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -15,6 +16,44 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, config, theme }) {
+      const sides = {
+        t: "Top",
+        r: "Right",
+        b: "Bottom",
+        l: "Left",
+      };
+
+      const createHighResBorderClass = (abbr: any, side: any) => {
+        const className = `.ot-border${abbr ? `-${abbr}` : ""}`;
+        const borderSide = side ? side : "";
+
+        return {
+          [className]: {
+            [`border${borderSide}Width`]: "1px",
+            borderColor: "#E9F1FF",
+            borderStyle: "solid",
+            [`@media (min-resolution: 192dpi), (min-resolution: 2dppx)`]: {
+              [`border${borderSide}Width`]: "0.5px",
+              borderColor: "#D4E4FF",
+            },
+          },
+        };
+      };
+
+      const componentStyles = {};
+
+      // Create high resolution border class for all sides
+      Object.assign(componentStyles, createHighResBorderClass(null, null));
+
+      // Create high resolution border classes for individual sides
+      for (const [abbr, side] of Object.entries(sides)) {
+        Object.assign(componentStyles, createHighResBorderClass(abbr, side));
+      }
+
+      addComponents(componentStyles);
+    }),
+  ],
 };
 export default config;
