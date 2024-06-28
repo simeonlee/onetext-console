40719e2f2b1c9bf72a50340b74dddadff20097c7
simeonlee <15081451+simeonlee@users.noreply.github.com>
2024-06-27 21:22:56 -0400

    step selection shows step details in right nav

--- src/components/nav/FlowEditorRightNav.tsx
+++ src/components/nav/FlowEditorRightNav.tsx
@@ -2,8 +2,15 @@ import {
   APP_DESKTOP_TOP_NAV_HEIGHT as topNavHeight,
   APP_DESKTOP_RIGHT_NAV_WIDTH as rightNavWidth,
 } from "~/constants/base";
+import { useFlowEditor } from "~/contexts/FlowEditorContext";
+import pizzaOrderFlow from "~/pages/pizzaOrderFlow.json";
 
 export default function FlowEditorRightNav() {
+  const { selectedStepId } = useFlowEditor();
+  const selectedStep = pizzaOrderFlow.steps.find(
+    (step) => step.id === selectedStepId
+  );
+
   return (
     <div
       className="hidden md:block relative overflow-y-scroll scrollbar-none ot-border-l ot-black"
@@ -15,7 +22,31 @@ export default function FlowEditorRightNav() {
       <div className="inline-block">
         <div className="px-6 transition-all pt-3 pb-4">
           <h4 className="text-[11px] text-gray-400 py-2 mb-0.5">Flows</h4>
-          <div className="space-y-1"></div>
+          {selectedStep && (
+            <div className="space-y-1">
+              <h5 className="text-[13px] text-gray-600">
+                Step ID: {selectedStep.id}
+              </h5>
+              <p className="text-[12px] text-gray-500">
+                Type: {selectedStep.type}
+              </p>
+              <p className="text-[12px] text-gray-500">
+                Message: {selectedStep.message}
+              </p>
+              {selectedStep.events && (
+                <div className="text-[12px] text-gray-500">
+                  <p>Subevents:</p>
+                  <ul className="list-disc list-inside">
+                    {selectedStep.events.map((event, index) => (
+                      <li key={index}>
+                        {event.intent} - Next Step: {event.nextStepID}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
