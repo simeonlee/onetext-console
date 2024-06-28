0429c5d231af9886252e345b96078753d8245d80
simeonlee <15081451+simeonlee@users.noreply.github.com>
2024-06-28 12:57:27 -0400

    delete nit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -222,7 +222,6 @@ function FlowEditor() {
                             step={nextNextNextStep}
                             isTerminal={isTerminal}
                           />
-                          // )
                         );
                       })
                     );
