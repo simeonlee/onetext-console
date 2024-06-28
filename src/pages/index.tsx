ba8c932eff9d3cf7d9e1e0ede6007261f8cc4cad
simeonlee <15081451+simeonlee@users.noreply.github.com>
2024-06-27 20:16:24 -0400

    increase flow editor bottom padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         }}
       >
         <div
-          className="overflow-scroll py-12 px-20 2xl:px-24 w-full"
+          className="overflow-scroll pt-12 pb-56 px-20 2xl:px-24 w-full"
           style={{ width: `calc(100vw - ${rightNavWidth}px)` }}
         >
           <div className="text-sm font-medium text-[#1A76FC]">Flows</div>
